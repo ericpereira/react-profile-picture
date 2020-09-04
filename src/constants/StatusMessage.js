86a4178085129e6bb58eac5ed033a25d5b7a8220
Eric Pereira <ericiclone@gmail.com>
2020-09-04 11:11:07 -0300

    status message pt-br

--- src/constants/StatusMessage.js
+++ src/constants/StatusMessage.js
@@ -5,24 +5,24 @@ import Icon from "../components/Icon/Icon";
 import style from "../components/Message/Message.scss"
 
 const StatusMessage = {
-  [Status.EMPTY]: <p className={style["message__text"]}>Drop your photo here or tap to select.</p>,
+  [Status.EMPTY]: <p className={style["message__text"]}>Solte sua foto aqui ou toque para selecionar.</p>,
   [Status.LOADING]:<Icon name="loading" size={48}/>,
   [Status.DRAGOVER]: (
     <Fragment>
       <Icon name="upload" size={48}/>
-      <p className={style["message__text"]}>Drop your photo</p>
+      <p className={style["message__text"]}>Solte sua foto</p>
     </Fragment>
   ),
   [Status.INVALID_FILE_TYPE]: (
     <Fragment>
-      <p className={style["message__text"]}>Only images allowed.</p>
-      <p className={style["message__text"]}>Drop your photo here or tap to select.</p>
+      <p className={style["message__text"]}>Apenas imagens permitidas.</p>
+      <p className={style["message__text"]}>Solte sua foto aqui ou toque para selecionar.</p>
     </Fragment>
   ),
   [Status.INVALID_IMAGE_SIZE]: (
     <Fragment>
-      <p className={style["message__text"]}>Your photo must be larger than 350px.</p>
-      <p className={style["message__text"]}>Drop your photo here or tap to select.</p>
+      <p className={style["message__text"]}>Sua foto deve ter mais de 350 px.</p>
+      <p className={style["message__text"]}>Solte sua foto aqui ou toque para selecionar.</p>
     </Fragment>
   ),
   [Status.LOADED]: null
